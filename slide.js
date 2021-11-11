7dd51e026deb3d0cb9096cd0e690eb2a08bada12
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-11-11 13:51:23 -0500

    Add files via upload
    slider functionality improved

--- slide.js
+++ slide.js
@@ -1,61 +1,118 @@
 const slides = document.querySelectorAll('.slide');
-const buttons = document.querySelectorAll(".button")
-const auto = true; // Auto scroll
-const intervalTime = 5000;
-let slideInterval;
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
 
 const nextSlide = () => {
-
-    const current = document.querySelector('.current');
-
-    current.classList.remove('current');
-
-    if (current.nextElementSibling) {
-
-        current.nextElementSibling.classList.add('current');
-    } else {
-
-        slides[0].classList.add('current');
-        buttons[0].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  
 };
 
 const prevSlide = () => {
-
-    const current = document.querySelector('.current');
-
-    current.classList.remove('current');
-
-    if (current.previousElementSibling) {
-
-        current.previousElementSibling.classList.add('current');
-    } else {
-
-        slides[slides.length - 1].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
 };
 
-
-buttons.addEventListener('click', e => {
-    nextSlide();
-    if (auto) {
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
+// Button events
+next.addEventListener('click', e => {
+  nextSlide();
+  
 });
 
 prev.addEventListener('click', e => {
-    prevSlide();
-    if (auto) {
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, intervalTime);
+  prevSlide();
+  
+  }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+const slides = document.querySelectorAll(".slide")
+const next = document.getElementById("#next");
+const prev = document.getElementById("#prev");
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+
+const nextSlide = () => {
+    const current = document.querySelector(".current")
+    current.classlist.remove("current")
+    if (current.nextElementSibling) {
+        current.nextElementSibling.classList.add("current")
+    } else {
+        slides[0].classList.add("current")
     }
-});
-
-
-if (auto) {
-
-    slideInterval = setInterval(nextSlide, intervalTime);
+    setTimeout(() => current.classList.remove("current"))
 }
+
+
+const prevSlide = () => {
+    const current = document.querySelector(".current")
+    current.classlist.remove("current")
+    if (current.previousElementSibling) {
+        current.previousElementSibling.classList.add("current")
+    } else{
+        slides[slides.length -1].classList.add("current")
+    }
+    setTimeout(() => current.classList.remove("current"))
+}
+
+next.addEventListener("click", e => {
+    nextSlide()
+})
+
+
+prev.addEventListener("click", e => {
+    prevSlide()
+})
+
+prevSlide() */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
