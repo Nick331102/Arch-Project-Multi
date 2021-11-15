7d741007e3ff0b253761e8a457262cf75f6656ff
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-11-15 07:58:54 -0500

    Add files via upload
    dark overlay when hambuger menu clicked added

--- nav.js
+++ nav.js
@@ -1,13 +1,15 @@
 const navSlide = () => {
 
 const burger = document.querySelector(".hamburger__menu")
-const navLinks = document.querySelector(".nav__links--container")
+const navLinks = document.querySelector(".nav__links--container");
+const overlay = document.querySelector(".clicked__overlay")
 
 
 
 burger.addEventListener('click', () => {
     navLinks.classList.toggle("show")
     burger.classList.toggle("clicked")
+    overlay.classList.toggle("cover")
    
 
 })
